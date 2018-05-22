9e9ac4103d1dd79af171b74417418b20031c7d26
NestorJacobo <nestor.jacobo@outlook.es>
2018-05-21 22:29:38 -0500

    pojo lista de compras en modulo Compras

--- SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/OrdenCompraPojo.java
+++ SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/OrdenCompraPojo.java
@@ -11,5 +11,67 @@ package pw.sap.pojo.Compras;
  */
 public class OrdenCompraPojo {
     
+    private String ord_folio;
+    private requisicionPojo requisicionpojo;
+    private Proveedores proveedores;
+    private String ord_fecha;
+    private String ord_pago;
+
+    public OrdenCompraPojo() {
+    }
+
+    public OrdenCompraPojo(String ord_folio, requisicionPojo requisicionpojo, Proveedores proveedores, String ord_fecha, String ord_pago) {
+        this.ord_folio = ord_folio;
+        this.requisicionpojo = requisicionpojo;
+        this.proveedores = proveedores;
+        this.ord_fecha = ord_fecha;
+        this.ord_pago = ord_pago;
+    }
+
+    public String getOrd_folio() {
+        return ord_folio;
+    }
+
+    public void setOrd_folio(String ord_folio) {
+        this.ord_folio = ord_folio;
+    }
+
+    public requisicionPojo getRequisicionpojo() {
+        return requisicionpojo;
+    }
+
+    public void setRequisicionpojo(requisicionPojo requisicionpojo) {
+        this.requisicionpojo = requisicionpojo;
+    }
+
+    public Proveedores getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(Proveedores proveedores) {
+        this.proveedores = proveedores;
+    }
+
+    public String getOrd_fecha() {
+        return ord_fecha;
+    }
+
+    public void setOrd_fecha(String ord_fecha) {
+        this.ord_fecha = ord_fecha;
+    }
+
+    public String getOrd_pago() {
+        return ord_pago;
+    }
+
+    public void setOrd_pago(String ord_pago) {
+        this.ord_pago = ord_pago;
+    }
+
+    @Override
+    public String toString() {
+        return ord_folio; //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
 }
--- SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/listacomra.java
+++ SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/listacomra.java
@@ -5,31 +5,68 @@ package pw.sap.pojo.Compras;
  * @author Marii
  */
 public class listacomra  {
-    private String folio_compra;
-    private String fecha_compra;
+    private String list_folio;
+    private OrdenCompraPojo ordencomprapojo;
+    private String list_fecha;
+    private String list_pago;
+    private String list_factura;
 
-    public String getFolio_compra() {
-        return folio_compra;
+    public listacomra() {
     }
 
-    public String getFecha_compra() {
-        return fecha_compra;
+    public listacomra(String list_folio, OrdenCompraPojo ordencomprapojo, String list_fecha, String list_pago, String list_factura) {
+        this.list_folio = list_folio;
+        this.ordencomprapojo = ordencomprapojo;
+        this.list_fecha = list_fecha;
+        this.list_pago = list_pago;
+        this.list_factura = list_factura;
     }
 
-    public void setFolio_compra(String folio_compra) {
-        this.folio_compra = folio_compra;
+    public String getList_folio() {
+        return list_folio;
     }
 
-    public void setFecha_compra(String fecha_compra) {
-        this.fecha_compra = fecha_compra;
+    public void setList_folio(String list_folio) {
+        this.list_folio = list_folio;
+    }
+
+    public OrdenCompraPojo getOrdencomprapojo() {
+        return ordencomprapojo;
+    }
+
+    public void setOrdencomprapojo(OrdenCompraPojo ordencomprapojo) {
+        this.ordencomprapojo = ordencomprapojo;
+    }
+
+    public String getList_fecha() {
+        return list_fecha;
+    }
+
+    public void setList_fecha(String list_fecha) {
+        this.list_fecha = list_fecha;
+    }
+
+    public String getList_pago() {
+        return list_pago;
+    }
+
+    public void setList_pago(String list_pago) {
+        this.list_pago = list_pago;
+    }
+
+    public String getList_factura() {
+        return list_factura;
+    }
+
+    public void setList_factura(String list_factura) {
+        this.list_factura = list_factura;
+    }
+
+    @Override
+    public String toString() {
+        return list_folio; //To change body of generated methods, choose Tools | Templates.
     }
     
-    
-    public listacomra(String folio, String fecha) {
-        this.folio_compra = folio;
-        this.fecha_compra= fecha;
-        
-    }
     
     
 }
