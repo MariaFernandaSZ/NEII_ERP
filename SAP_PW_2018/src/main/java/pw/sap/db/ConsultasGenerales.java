cc4bfae4b735303b7900ed2af86fa0d66d617e21
montse <montse@montse-Inspiron-5567>
2018-05-27 02:31:01 -0500

    consulta merma

--- SAP_PW_2018/src/main/java/pw/sap/db/ConsultasGenerales.java
+++ SAP_PW_2018/src/main/java/pw/sap/db/ConsultasGenerales.java
@@ -13,6 +13,7 @@ import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.Properties;
 import pw.sap.pojo.Contabilidad.Calen_Contable;
+import pw.sap.pojo.Inventarios.Merma;
 
 /**
  *
@@ -45,4 +46,28 @@ public class ConsultasGenerales {
         return l;
     }
     
+    public static LinkedList consultaGMermac() throws SQLException, ClassNotFoundException {        
+        Connection conn;
+        Class.forName("org.postgresql.Driver");
+        LinkedList <Merma> l=new LinkedList<Merma>();
+        Properties connProp = new Properties();
+        connProp.put("user", "postgres");
+        connProp.put("password", "root");
+        conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/BDSAPPW", connProp);
+        Statement stmt;        
+        stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM merma where tipo_merma='MermaConocida' order by fecha_merma asc");
+            while (rs.next()) {
+                Merma cc=new Merma();
+                cc.setId_producto(rs.getString("id_producto"));
+                cc.setIDMerma(rs.getInt("id_merma"));
+                cc.setFechaRe(rs.getString("fecha_merma"));                
+                cc.setTipo(rs.getString("tipo_merma"));
+                cc.setCantidad(rs.getInt("cantidad_merma"));
+                cc.setObs(rs.getString("motivo"));                
+                l.add(cc);
+            }                    
+        conn.close();
+        return l;
+    }
 }
