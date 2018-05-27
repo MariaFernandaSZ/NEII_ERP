f279bc8147d87d14c684aa07d9e3918d3a33b215
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-27 01:59:56 -0500

    Update PlanDeCuentas.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/PlanDeCuentas.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/PlanDeCuentas.java
@@ -56,7 +56,8 @@ public class PlanDeCuentas extends HttpServlet {
         request.getSession().setAttribute("clase_cuenta", l.get(4));
         request.getSession().setAttribute("clase_sat", l.get(5));
         request.getSession().setAttribute("naturaleza", l.get(6));
-
+int i = c.insercionRegistro((int)request.getSession().getAttribute("usuario"), (String)request.getSession().getAttribute("area"), "Consulta del plan de cuentas");
+        
         response.sendRedirect("Contabilidad/plan_cuentas.jsp");
 
     }
