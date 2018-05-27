1263886f115f930a5c0c5e2c4df536f5b0a82f21
Mauricio Eduardo Carrillo Vázquez <mauecarrillov@outlook.com>
2018-05-27 13:50:54 -0500

    Continuacion Servlet Excel

--- SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/ExcelReportes.java
+++ SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/ExcelReportes.java
@@ -12,6 +12,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.sl.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -41,13 +43,26 @@ public class ExcelReportes
                 
     }
     
-    public static void cearExcelNuevo(){
+    public static void crearExcelNuevo(){
         Workbook libro = new XSSFWorkbook();
         org.apache.poi.ss.usermodel.Sheet hoja = libro.createSheet("Reporte Compras");
         
+        //Creacion de filas y cledas relacionadas con la fila
+        Row fila = hoja.createRow(5);
+        fila.createCell(6).setCellValue("Primer Reporte");
+        fila.createCell(2).setCellValue(780505.2);
+        fila.createCell(3).setCellValue(true);
+        
+        //Celda por separado para formulas, para dar mas caracteristicas a la celda
+        Cell celda = fila.createCell(7);
+        celda.setCellFormula(String.format("1+1", ""));
+        
+        
+        
+        
         try
         {
-            FileOutputStream archivo = new FileOutputStream("C:\\Users\\Adrian\\Escritorio\\excel\\Reporte Compras Productos.xlsx");
+            FileOutputStream archivo = new FileOutputStream("C:\\Users\\Adrian\\Escritorio\\excel\\Reporte Productos.xlsx");
             libro.write(archivo);
             archivo.close();
         } catch (FileNotFoundException ex)
@@ -56,8 +71,7 @@ public class ExcelReportes
         } catch (IOException ex)
         {
             Logger.getLogger(ExcelReportes.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-                
+        }               
     }
+    
 }
