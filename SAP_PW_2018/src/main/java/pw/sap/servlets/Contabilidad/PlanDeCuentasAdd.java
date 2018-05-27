6a2e24eaa4fa6159ee9635e7f72278396acfec10
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-27 02:00:23 -0500

    Update PlanDeCuentasAdd.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/PlanDeCuentasAdd.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/PlanDeCuentasAdd.java
@@ -47,7 +47,7 @@ public class PlanDeCuentasAdd extends HttpServlet {
         
         c.agregaPlan(Integer.parseInt(request.getParameter("idsat")), descripcion, request.getParameter("tipocuenta"), request.getParameter("clasecuenta"), request.getParameter("naturaleza"));
         
-//int i = c.insercionRegistro((int)request.getSession().getAttribute("usuario"), (String)request.getSession().getAttribute("area"), "Se agrego un plan de cuenta");
+        int i = c.insercionRegistro((int)request.getSession().getAttribute("usuario"), (String)request.getSession().getAttribute("area"), "Se agrego un plan de cuenta");
         
          response.sendRedirect("Contabilidad/plan_cuentas.jsp");
         
