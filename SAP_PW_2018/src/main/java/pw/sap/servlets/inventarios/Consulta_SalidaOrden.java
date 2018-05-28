1953161f683df2f7a895c824fcf5228d286411fa
Montserrath Olivo <38671806+MontserrathOG@users.noreply.github.com>
2018-05-28 00:38:27 -0500

    Update Consulta_SalidaOrden.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/inventarios/Consulta_SalidaOrden.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/inventarios/Consulta_SalidaOrden.java
@@ -40,14 +40,14 @@ public class Consulta_SalidaOrden extends HttpServlet {
         
         Conexion c=new Conexion();
         
-        ArrayList l=c.consultaSalidasId(Integer.parseInt(request.getParameter("id_ordenventa")));
+        ArrayList l=c.consultaSalidasOrden(Integer.parseInt(request.getParameter("id_ordenventa")));
         request.getSession().setAttribute("id_producto", l.get(0));
         request.getSession().setAttribute("id_ordenventa", l.get(1));
         request.getSession().setAttribute("cantidad_vendida", l.get(2));
         request.getSession().setAttribute("total_mon", l.get(3));
-       int i = c.insercionRegistro((int)request.getSession().getAttribute("usuario"), (String)request.getSession().getAttribute("area"), "Se realizo una consulta de salida de orden");
+       //int i = c.insercionRegistro((int)request.getSession().getAttribute("usuario"), (String)request.getSession().getAttribute("area"), "Se realizo una consulta de salida de orden");
          
-        request.getRequestDispatcher("Inventarios/Inventario/salidas.jsp").forward(request, response);
+        request.getRequestDispatcher("Inventarios/Inventario/SalidasporOrd.jsp").forward(request, response);
         
         
     }
