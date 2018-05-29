8b34b2c85a9fc88d54b05b2384abc12c62e02ef8
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 21:38:34 -0500

    Update Servlet_consultaTarea.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_consultaTarea.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_consultaTarea.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import pw.sap.db.Conexion;
