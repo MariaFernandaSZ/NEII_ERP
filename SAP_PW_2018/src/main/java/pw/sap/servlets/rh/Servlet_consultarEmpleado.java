97832313e23d0c5d72c556af2833c2b992ce0be4
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 21:38:59 -0500

    Update Servlet_consultarEmpleado.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_consultarEmpleado.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_consultarEmpleado.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
