ac9f7576750853987a0be33492131b04ceed7670
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-29 01:09:20 -0500

    Update QuerysProducto.java

--- SAP_PW_2018/src/main/java/pw/sap/pojo/Inventarios/QuerysProducto.java
+++ SAP_PW_2018/src/main/java/pw/sap/pojo/Inventarios/QuerysProducto.java
@@ -89,6 +89,16 @@ public class QuerysProducto {
                   
         }
     
+    public int insercionRegistro(int id_emp, String area, String des) throws SQLException{
+        openDB();
+        int valor=1;
+        PreparedStatement ps;
+        ps=conn.prepareStatement("INSERT INTO log(id_emp,area,des) VALUES ("+id_emp+",'"+area+"','"+des+"');");
+        valor= ps.executeUpdate();
+        closeDB();        
+        return valor;
+    }
+    
     
    
 }
