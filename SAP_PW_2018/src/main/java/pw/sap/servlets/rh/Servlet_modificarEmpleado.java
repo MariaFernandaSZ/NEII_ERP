70ec12ab6389ee59a0b6081554e03832a6e5caf4
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 21:40:51 -0500

    Update Servlet_modificarEmpleado.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_modificarEmpleado.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_modificarEmpleado.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
