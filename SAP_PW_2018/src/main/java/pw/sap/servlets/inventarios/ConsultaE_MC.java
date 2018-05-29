f841c72d08b81e321f291cc6cc0a5c30299e0c1b
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 22:38:36 -0500

    Update ConsultaE_MC.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/inventarios/ConsultaE_MC.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/inventarios/ConsultaE_MC.java
@@ -11,6 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
