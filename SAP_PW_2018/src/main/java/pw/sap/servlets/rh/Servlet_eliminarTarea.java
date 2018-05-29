d3ec89e3ed2432e0a12581f7d0cbfa882cb6cb80
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 21:39:51 -0500

    Update Servlet_eliminarTarea.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_eliminarTarea.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/rh/Servlet_eliminarTarea.java
@@ -10,6 +10,7 @@ import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.servlet.http.HttpSession;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
