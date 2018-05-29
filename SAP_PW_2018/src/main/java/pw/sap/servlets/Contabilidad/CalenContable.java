2b096fd5a671eac84da904d15f1abc1119baf26f
EmilioGorostieta <milogorostieta071297@hotmail.com>
2018-05-28 20:45:41 -0500

    Update CalenContable.java

--- SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/CalenContable.java
+++ SAP_PW_2018/src/main/java/pw/sap/servlets/Contabilidad/CalenContable.java
@@ -50,7 +50,11 @@ public class CalenContable extends HttpServlet {
         HttpSession sesion=request.getSession(true);
         System.out.println("sesion usuario:"+sesion.getAttribute("usuario"));
         System.out.println("sesion usuario:"+sesion.getAttribute("area"));
-        c.insercionRegistro((int)sesion.getAttribute("usuario"), (String)sesion.getAttribute("area"), "Se realizo una consulta al calendario");
+         //registro para log
+        HttpSession sesion=request.getSession(true);
+        System.out.println("sesion usuario:"+sesion.getAttribute("usuario"));
+        System.out.println("sesion usuario:"+sesion.getAttribute("area"));
+        c.insercionRegistro((int)sesion.getAttribute("usuario"), (String)sesion.getAttribute("area"), "Consulta de Calendarios contables");        
         
         response.sendRedirect("Contabilidad/calen_contable_1.jsp");
     }
