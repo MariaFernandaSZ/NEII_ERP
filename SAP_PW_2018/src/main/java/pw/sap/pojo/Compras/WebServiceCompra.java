bf7be8e35afaba02ada4838f09e81140461b2b04
Mauricio Eduardo Carrillo Vázquez <mauecarrillov@outlook.com>
2018-06-06 21:29:07 -0500

    Correccio ServiceCompra

--- SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/WebServiceCompra.java
+++ SAP_PW_2018/src/main/java/pw/sap/pojo/Compras/WebServiceCompra.java
@@ -8,19 +8,20 @@ import java.util.ArrayList;
  */
 public class WebServiceCompra {
     
-    public String servicio(String cliente, String cuenta, String monto){
-        String servicio;
-        while(monto.length() < 18){monto = "0" + monto;}
-        servicio = cliente + cuenta + monto;
-        return servicio;
+    public String serviciocompra(String codigo_prod, String cantidad){
+        String serviciocompra;
+        while(codigo_prod.length() < 18){codigo_prod = "0" + codigo_prod;}
+        while(cantidad.length() < 8){cantidad = "0" + cantidad;}
+        serviciocompra = codigo_prod+cantidad;
+        return serviciocompra;
     }
     
-    public String conversion(String numero){
-        String cadena = numero;
-        ArrayList<String> lista = new <String> ArrayList();
-        cadena.replace("-","\0");
-   
-        cadena = lista.get(0) + lista.get(1);
-        return cadena;
+    public String fecha(String rfc_prov, String cod_cantidad, String fecha){
+        String serviciocompra;
+        String cadena = fecha;
+//        ArrayList<String> lista = new <String> ArrayList();
+        cadena = cadena.replace("-","");
+        serviciocompra = rfc_prov+cod_cantidad+cadena;
+        return serviciocompra;
     }
 }
